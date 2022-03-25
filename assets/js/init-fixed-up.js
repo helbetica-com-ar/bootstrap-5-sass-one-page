9f9cb81578c45e478b8c08eee7cf24493bfe097d
helbetica-com-ar <helbetica@outlook.com>
2022-03-25 03:42:12 -0300

    changed form style to input-group,  added content

--- assets/js/init-fixed-up.js
+++ assets/js/init-fixed-up.js
@@ -60,3 +60,26 @@ function customScrollTo(idOrClass) {
         $('html,body').animate({ scrollTop: $('.' + idOrClass).offset().top - 92 }, 30);
     }
 }
+
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                    form.classList.add('form-invalid')
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
